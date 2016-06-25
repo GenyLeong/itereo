33b13b287c602ed923d35472775278b3dda0d4ec
genyleong <leongenesis0@gmail.com>
2016-06-25 18:46:28 -0500

    comentarios

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 (function ($) {
     function getKeys(obj) {
+    	//esta variable es un objeto que abstrae los datos para usarlos en el objeto que hereda de ese prototipo
     	var hasOwn = Object.prototype.hasOwnProperty;
         var keys = [];
         var name;
-
+        //prototipo de lo que vamos a buscar
         for (name in obj) {
             if (hasOwn.call(obj, name)) {
                 keys.push(name);
@@ -16,15 +17,16 @@
     $(document).ready(function() {
 
         var emptyValue = "-"
-
+//localizamos el json desde un servidor para eso tenemos que crear un archivo server.js que nos permita acceder de red a red
     	$.ajax({
 			url: "http://localhost:1450/data",
 			dataType: "json",
 		})
 		.done(function (data) {
 			var i;
+			//utilizamos la funcion que creamos para ello(2)
  			var keys = getKeys(data);
-
+//aqui empieza realmente el ejercicio, si Services existe recorrerá la data hasta encontrar las variables requeridas (1)
 			if(keys.indexOf("Services") != -1){
 				var $table = $("#table tbody");
 
@@ -38,17 +40,17 @@
 				var to_date;
 
 				var template;
-
+//cuando encuentre Services, recorrerá en un array cada elemento del bucle
 				for(i = 0; i < services.length; i++){
 					service = services[i];
 					keys = getKeys(service);
-
+//id's de servicios a buscar
 					ser_name = emptyValue;
 					svt_id = emptyValue;
 					ngt_qty = emptyValue;
 					date = emptyValue;
 					to_date = emptyValue;
-
+//el elemento service busca "ser_name" si lo encuentra lo almacena en otra variable
 					if (keys.indexOf("ser_name") != -1){
 						ser_name = service["ser_name"];
 					}
@@ -56,7 +58,7 @@
 					if (keys.indexOf("svt_id") != -1){
 						svt_id = service["svt_id"];
 						svt_id = parseFloat(svt_id);
-
+//cuando el elemento está vacío se muestra la variable sgte
 						if(isNaN(svt_id)){
 							svt_id=emptyValue;
 						}
@@ -75,7 +77,7 @@
 						date = service["date"];
 						date = new Date(date);
 					}
-
+//restricciones
 					if(svt_id===0 || svt_id===7){
 						to_date = date.getTime() + ngt_qty*24*60*60*1000;
 						to_date = new Date(to_date);
@@ -87,7 +89,7 @@
 
 					date = dateFormat(date, "dd mmm");
 
-
+//pintandolo en el html
 
 					template = "<tr>" +
 						"<td>" + ser_name + "</td>" + 
